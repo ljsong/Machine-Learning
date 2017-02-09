6e14808a147f7c509aadb3d93ccc04bab06f7f65
songlongjun <songlongjun@pset.suntec.net>
2017-02-09 18:10:22 +0800

    implementing CNN

--- src/cnn/conv_synapse.py
+++ src/cnn/conv_synapse.py
@@ -2,45 +2,159 @@
 # -*- coding: utf-8 -*-
 
 from numpy.random import normal
-# from im2col import im2col
+from numpy import maximum
+from numpy import concatenate
+from numpy import ones
+from numpy import sum
+from im2col import im2col
+from im2col import col2im
 
 
-class ConvSyanpse(object):
+class Synapse(object):
+    """
+    Interface of kinds of synapses
+    """
+    def __init__(self):
+        pass
 
-    def __init__(self, kernel_size, input_layer, output_layer):
+    def active(self):
+        pass
+
+    def derivative(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
+
+
+class ConvSyanpse(Synapse):
+
+    def __init__(self, input_layer, kernel_size, kernel_cnt, padding=1, stride=1):
         """
         This class define a convolutional layer which has a filter matrix,
         input vector and output vector
         :param kernel_size: this should be a number which indicate the size of filter matrix
+        :param kernel_cnt: the number of the filters
         :param input_layer: this vector store the value of input which should be 3-D image
-        :param output_layer: convolutional sum
+        :param padding: how many zeros should be padded around the input value
+        :param stride: the step which kernel go through the input value
         """
         self.input_layer = input_layer
-        self.output_layer = output_layer
-        depth = input_layer.shape[0]
+        self.kernel_size = kernel_size
+        self.kernel_cnt = kernel_cnt
+        self.padding = padding
+        self.stride = stride
 
-        self.kernel = normal(size=(depth, kernel_size, kernel_size))
+        # input_layer after transforming by im2col
+        self.input_cols = None
+        self.output_layer = None
+
+        depth, height, width = input_layer.shape
+        # each row is a filter
+        self.kernel = normal(size=(self.kernel_cnt, depth, self.kernel_size, self.kernel_size))
+        self.bias = ones((self.kernel_cnt, 1))
+
+        self.bias_delta = None
+        self.kernel_delta = None
+
+        # the counts of receptive filed
+        self.rf_height = (height - self.kernel_size + 2 * self.padding) / self.stride + 1
+        self.rf_width = (width - self.kernel_size + 2 * self.padding) / self.stride + 1
+
+    def active(self):
+        inputs = self.input_layer.reshape(1, self.input_layer.shape)
+        self.input_cols = im2col(inputs, self.kernel_size,
+                                 self.kernel_size, self.padding, self.stride)
+
+        kernel_cols = self.kernel.reshape(self.kernel_cnt, -1)
+        conv_sum = kernel_cols.dot(self.input_cols) + self.bias
+        conv_sum = conv_sum.reshape(self.kernel_cnt, self.rf_height, self.rf_width, 1)
+
+        conv_sum = conv_sum.transpose(3, 0, 1, 2)
+        return conv_sum
+
+    def forward(self):
+        self.output_layer = self.active()
+
+    def backward(self, error):
+        self.bias_delta = sum(error, axis=(0, 2, 3))
+        self.bias_delta = self.bias_delta.reshape(self.kernel_cnt, -1)
+
+        error_cols = error.transpose(1, 2, 3, 0).reshape(self.kernel_cnt, -1)
+        self.kernel_delta = error_cols.dot(self.input_cols.T)
+        self.kernel_delta = self.kernel_delta(self.kernel.shape)
+
+        kernel_cols = self.kernel.reshape(self.kernel_cnt, -1)
+        prev_error_cols = kernel_cols.T.dot(error_cols)
+        prev_error = col2im(prev_error_cols, self.input_layer.shape,
+                            self.kernel_size, self.kernel_size,
+                            padding=self.padding, stride=self.stride)
+
+        return prev_error
 
 
-class ReLUSynapse(object):
+class ReLUSynapse(Synapse):
 
-    def __init__(self):
-        pass
+    def __init__(self, input_layer):
+        self.input_layer = input_layer
+        self.output_layer = None
+
+    def active(self):
+        self.output_layer = maximum(self.input_layer, 0)  # here 0 has been broadcast
+
+    def derivative(self):
+        return 1
 
 
-class PoolingSynapse(object):
+class PoolingSynapse(Synapse):
 
-    def __init__(self, pool_size):
-        pass
+    def __init__(self, input_layer, pool_size, stride):
+        self.input_layer = input_layer
+        self.output_layer = None
+        self.pool_size = pool_size
+        self.stride = stride
+
+        depth, height, width = self.input_layer.shape
+        self.rf_width = (width - self.pool_size) * self.stride + 1
+        self.rf_height = (height - self.pool_size) * self.stride + 1
+
+    def pooling(self, func):
+        depth, height, width = self.input_layer.shape
+
+        for ix in range(depth):
+            matrix = self.input_layer[ix, :, :].reshape(height, width)
+            cols = im2col(matrix, self.pool_size, self.pool_size, 0, self.stride)
+
+            pooling_matrix = func(cols)
+            pooling_matrix = pooling_matrix.reshape((self.rf_height, self.rf_width, 1))
+            if not self.output_layer:
+                self.output_layer = pooling_matrix
+            else:
+                self.output_layer = concatenate((self.output_layer, pooling_matrix), axis=0)
 
 
 class MaxPoolingSynapse(PoolingSynapse):
 
-    def __init__(self, pool_size):
-        super(MaxPoolingSynapse, self).__init__(pool_size)
+    def __init__(self, input_layer, output_layer, pool_size, stride):
+        """
+        MaxPooling select the maximum value of a matrix, we should remember the
+        index of the maximum one and use it to back propagate the error to the
+        correct neuron
+        :param pool_size: the size of pooling layer
+        """
+        super(MaxPoolingSynapse, self).__init__(input_layer, output_layer, pool_size, stride)
+
+    def pooling(self):
+        pass
 
 
 class AvgPoolingSynapse(PoolingSynapse):
 
-    def __init__(self, pool_size):
-        super(AvgPoolingSynapse, self).__init__(pool_size)
+    def __init__(self, input_layer, output_layer, pool_size, stride):
+        super(AvgPoolingSynapse, self).__init__(input_layer, output_layer, pool_size, stride)
+
+    def pooling(self):
+        pass
--- src/cnn/im2col.py
+++ src/cnn/im2col.py
@@ -7,17 +7,18 @@ import numpy as np
 def get_im2col_indices(
         x_shape, field_height, field_width, padding=1, stride=1):
     # First figure out what the size of the output should be
-    w = x_shape[-1]
-    h = x_shape[-2]
+    # number means the counts of image, channel usually is 3(RGB)
+    number, channel, height, width = x_shape
 
-    assert (h + 2 * padding - field_height) % stride == 0
-    assert (w + 2 * padding - field_height) % stride == 0
-    out_height = (h + 2 * padding - field_height) / stride + 1
-    out_width = (w + 2 * padding - field_width) / stride + 1
+    assert (height + 2 * padding - field_height) % stride == 0
+    assert (width + 2 * padding - field_height) % stride == 0
+    out_height = int((height + 2 * padding - field_height) / stride + 1)
+    out_width = int((width + 2 * padding - field_width) / stride + 1)
 
     # field_height * field_width rows
     i0 = np.repeat(np.arange(field_height), field_width)
-    j0 = np.tile(np.arange(field_width), field_height)
+    i0 = np.tile(i0, channel)
+    j0 = np.tile(np.arange(field_width), field_height * channel)
 
     # out_height * out_width columns
     i1 = stride * np.repeat(np.arange(out_height), out_width)
@@ -26,27 +27,53 @@ def get_im2col_indices(
     # broadcasting
     i = i0.reshape(-1, 1) + i1.reshape(1, -1)
     j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+    k = np.repeat(np.arange(channel), field_height * field_width).reshape(-1, 1)
 
-    return i, j
+    return i.astype(int), j.astype(int), k.astype(int)
 
 
 def im2col(x, field_height, field_width, padding=1, stride=1):
     """ An implementation of im2col based on some fancy indexing """
     # Zero-pad the input
     p = padding
-    x_padded = np.pad(x, ((p, p), (p, p)), mode='constant')
+    x_padded = np.pad(x, ((0, 0), (0, 0), (p, p), (p, p)), mode='constant')
 
-    indices = get_im2col_indices(
+    i, j, k = get_im2col_indices(
         x.shape, field_height, field_width, padding, stride)
 
-    cols = x_padded[indices]
+    cols = x_padded[:, k, i, j]
+    # channel(or depth) means the counts of color, usually it is 3(RGB)
+    channel = x.shape[1]
+    cols = cols.transpose(1, 2, 0).reshape(field_height * field_width * channel, -1)
 
     return cols
 
 
+def col2im(cols, x_shape, field_height, field_width, padding=1, stride=1):
+    number, channel, height, width = x_shape
+    padded_h = height + 2 * padding
+    padded_w = width + 2 * padding
+
+    x_padded = np.zeros((number, channel, padded_h, padded_w), dtype=cols.dtype)
+    i, j, k = get_im2col_indices(x_shape, field_height, field_width, padding, stride)
+
+    cols_reshaped = cols.reshape(channel * field_height * field_width, -1, number)
+    cols_reshaped = cols_reshaped.transpose(2, 0, 1)
+    np.add.at(x_padded, (slice(None), k, i, j), cols_reshaped)
+
+    if padding == 0:
+        return x_padded
+
+    return x_padded[:, :, padding:-padding, padding:-padding]
+
+
 def main():
-    a = np.eye(3, dtype=int)
-    print im2col(a, 2, 2)
+    a = np.arange(8).reshape((1, 1, 4, 2))
+    cols = im2col(a, 2, 2)
+    print cols
+    b = col2im(cols, a.shape, 2, 2)
+
+    print b
 
 
 if __name__ == '__main__':
